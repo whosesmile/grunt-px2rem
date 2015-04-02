699347e6197e9b9faa0716ed41909ea26f020036
whosesmile <lovemoon@yeah.net>
2015-04-02 13:36:09 +0800

    1.add px2rem options 2.convert logic finished

--- tasks/px2rem.js
+++ tasks/px2rem.js
@@ -8,44 +8,52 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('px2rem', 'Convert px to rem for mobile html5 page', function() {
+  grunt.registerMultiTask('px2rem', 'Convert px to rem for mobile html5 page', function () {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       punctuation: '.',
-      separator: ', '
+      separator: ', ',
+      ignore0: true,
+      ignore1: true,
+      root: 32
     });
 
     // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // console.log(f)
-      // // Concat specified files.
-      // var src = f.src.filter(function(filepath) {
-      //   // Warn on and remove invalid source files (if nonull was set).
-      //   if (!grunt.file.exists(filepath)) {
-      //     grunt.log.warn('Source file "' + filepath + '" not found.');
-      //     return false;
-      //   } else {
-      //     return true;
-      //   }
-      // }).map(function(filepath) {
-      //   // Read file source.
-      //   return grunt.file.read(filepath);
-      // }).join(grunt.util.normalizelf(options.separator));
+    this.files.forEach(function (f) {
+      // Concat specified files.
+      var src = f.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        }
+        else {
+          return true;
+        }
+      });
 
-      // // Handle options.
-      // src += options.punctuation;
+      var sources = src.map(function (filepath) {
+        return grunt.file.read(filepath);
+      }).join('\n').replace(/\b\d+px\b/ig, function (match) {
+        if (options.ignore0 && '0px' === match.toLowerCase()) {
+          return match;
+        }
+        if (options.ignore1 && '1px' === match.toLowerCase()) {
+          return match;
+        }
+        return parseInt(match) / parseInt(options.root, 10) + 'rem';
+      });
 
-      // // Write the destination file.
-      // grunt.file.write(f.dest, src);
+      // Write the destination file.
+      grunt.file.write(f.dest, sources);
 
-      // // Print a success message.
-      // grunt.log.writeln('File "' + f.dest + '" created.');
-      grunt.log.writeln('px2rem:"' + f);
+      // Print a success message.
+      grunt.log.writeln('px2rem: ' + f.dest);
     });
   });
 
